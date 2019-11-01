d1c64a7785e17a70e70cfb4da334e72ba440d307
Emilie <emiholtz@iu.edu>
2019-11-01 13:40:59 -0400

    Initial Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //http://www.unitygeek.com/unity_c_singleton/
+    public static GameManager ins;
+    [HideInInspector]
+    public Node currentNode;
+
+    private void Awake()
+    {
+        if (ins != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            ins = this;
+            //DontDestroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        //allow the player to 'back up' from a node by pressing right click
+        if (Input.GetMouseButtonDown(1) && currentNode.GetComponent<Prop>() != null)
+        {
+            currentNode.GetComponent<Prop>().loc.MoveToNode();
+        }
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class Node : MonoBehaviour
+{
+    public Transform camPos;
+    public List<Node> reachableNodes = new List<Node>();
+
+    [HideInInspector]
+    public Collider col;
+
+    void Start()
+    {
+        col = GetComponent<Collider>();
+    }
+
+    void OnMouseDown()
+    {
+        MoveToNode();
+    }
+
+    public void MoveToNode()
+    {
+        //leave the existing node
+        if(GameManager.ins.currentNode != null)
+        {
+            GameManager.ins.currentNode.LeaveNode();
+        }
+
+        ///set current node
+        GameManager.ins.currentNode = this;
+
+        //move camera
+        Camera.main.transform.position = camPos.position;
+        Camera.main.transform.rotation = camPos.rotation;
+
+        //turn off our own collider
+        if(col != null)
+        {
+            print(col);
+            col.enabled = false;
+        }
+
+        //turn on all reachable node colliders
+        foreach (Node node in reachableNodes)
+        {
+            //print(node);
+            if(node.col != null)
+            {
+                col.enabled = true;
+            }
+        }
+    }
+
+    public void LeaveNode()
+    {
+        //turn off all reachable node colliders
+        foreach (Node node in reachableNodes)
+        {
+            if(node.col != null)
+            {
+                col.enabled = false;
+            }
+        }
+    }
+
+}
