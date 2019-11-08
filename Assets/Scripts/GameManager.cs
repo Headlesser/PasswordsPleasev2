06ae79ff384020f866b96d9a0055bd1f24a55f68
emilie <emiholtz@iu.edu>
2019-11-08 18:20:22 -0500

    Added in small painting assets, starting room setup (4 quadrants)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,10 +6,12 @@ public class GameManager : MonoBehaviour
 {
     //http://www.unitygeek.com/unity_c_singleton/
     public static GameManager ins;
-    [HideInInspector]
+    //[HideInInspector]
     public Node currentNode;
 
     public List<string> Inventory = new List<string>();
+    [HideInInspector]
+    public Node start;
 
     private void Awake()
     {
@@ -24,12 +26,20 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    void Start()
+    {
+    }
+
     void Update()
     {
         //allow the player to 'back up' from a node by pressing right click
         if (Input.GetMouseButtonDown(1) && currentNode.GetComponent<Prop>() != null)
         {
             currentNode.GetComponent<Prop>().loc.MoveToNode();
+        }
+        if(Input.GetMouseButtonDown(1) && currentNode.GetComponent<Location>().loc != null && currentNode.GetComponent<Prop>() == null)
+        {
+            currentNode.GetComponent<Location>().loc.MoveToNode();
         }
     }
 }
