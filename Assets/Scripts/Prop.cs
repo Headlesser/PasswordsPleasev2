8e315f28ecca8dd236f6ba9ca87dfb7344cffdad
Ferlost <laencyclopedia@gmail.com>
2019-11-11 14:55:52 -0500

    Re-merging our stuff hold up

--- Assets/Scripts/Prop.cs
+++ Assets/Scripts/Prop.cs
@@ -5,4 +5,12 @@ using UnityEngine;
 public class Prop : Node
 {
     public Location loc;
+    public string name;
+    public Sprite sprite;
+
+    public void Start()
+    {
+        if (name == null)
+            name = gameObject.name;
+    }
 }
