f5dddbcb8d296b6a0c87d1c9eaaad6471939079b
Kaleb Eberhart <kalebeberhart@gmail.com>
2019-12-14 16:14:25 -0500

    Added UI element for dialogue to appear and basic functioning dialogue

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject speechPanel;
+    public Text speechText;
+    public GameObject continueText;
+    public string targetSpeech = "";
+
+    public bool isSpeaking { get { return speaking != null; } }
+    [HideInInspector] public bool isWaitingForUserInput = false;
+    
+    Coroutine speaking = null;
+
+    public string[] speech;
+    int i = 0;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(i < speech.Length)
+            {
+                Say(speech[i]);
+            }
+            else
+            {
+                Close();
+            }
+
+            i++;
+        }
+    }
+
+    ///<summary>
+    ///Say something and show it on the speech box.
+    /// </summary>
+    public void Say(string speech)
+    {
+        //stop the speaking coroutine before moving onto the next line
+        StopSpeaking();
+
+        speaking = StartCoroutine(Speaking(speech, false));
+    }
+
+    public void StopSpeaking()
+    {
+        //check if speaker is speaking
+        if(isSpeaking)
+        {
+            StopCoroutine(speaking);
+        }
+
+        speaking = null;
+    }
+
+    IEnumerator Speaking(string speech, bool additive)
+    {
+        speechPanel.SetActive(true);
+        targetSpeech = speech;
+        continueText.SetActive(false);
+
+        if (!additive)
+        {
+            speechText.text = "";
+        }
+        else
+        {
+            targetSpeech = speechText.text + targetSpeech;
+        }
+
+        isWaitingForUserInput = false;
+
+        while(speechText.text != targetSpeech)
+        {
+            speechText.text += targetSpeech[speechText.text.Length];
+            yield return new WaitForEndOfFrame();
+        }
+       
+        continueText.SetActive(true);
+
+        //text finished
+        isWaitingForUserInput = true;
+
+        while(isWaitingForUserInput)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        StopSpeaking();
+    }
+
+     public void Close()
+    {
+        StopSpeaking();
+        speechPanel.SetActive(false);
+    }
+}
