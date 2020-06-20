2d138c4d7a852e528bdf76709610b52c2735e9ae
Emilie <emiholtz@iu.edu>
2020-06-20 14:07:36 -0400

    working on fixing security question screen bug.

---
+++ Assets/Scripts/SecurityQuestionChecker.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SecurityQuestionChecker : MonoBehaviour
+{
+    public string attempt;
+    public string password;
+    public GameObject input;
+    public GameObject userName;
+    public GameObject incorrectPassword;
+    public GameObject window;
+    public AudioSource audio;
+    public AudioClip incorrectEntryClip;
+    public GameObject secondWindow;
+    private bool allAnswered;
+    private int countAnswer = 0;
+
+    public void CheckAnswers()
+    {
+        Debug.Log(countAnswer);
+        if(countAnswer == 3)
+        {
+            allAnswered = true;
+        }
+    }
+
+    public void CheckPassword()
+    {
+        Debug.Log("Is this even working");
+        attempt = GetComponent<InputField>().text;
+        if (password.Equals(attempt))
+        {
+            input.SetActive(false);
+            userName.SetActive(false);
+            window.SetActive(true);
+            countAnswer += 1;
+            CheckAnswers();
+            if(secondWindow != null && allAnswered)
+            {
+                secondWindow.SetActive(true);
+                GameManager.gameManager.openedPC = true;
+                GameManager.gameManager.Monologue(GameManager.gameManager.currentNode);
+            }
+            GetComponent<AudioSource>().Play();
+            incorrectPassword.SetActive(false);
+        }
+        else
+        {
+            GetComponent<InputField>().text = "";
+            incorrectPassword.SetActive(true);
+            audio.PlayOneShot(incorrectEntryClip);
+        }
+    }
+
+    public void KeyboardClick()
+    {
+        Debug.Log("should be making typing noise?");
+        audio.pitch = Random.Range(1f, 1.2f);
+        audio.Play();
+    }
+}
