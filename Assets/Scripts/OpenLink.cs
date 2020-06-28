de014da63b262e78276a98c7b1a118b4d2503203
Emilie <emiholtz@iu.edu>
2020-06-28 12:08:18 -0400

    Added links to IU resources

--- Assets/Scripts/OpenLink.cs
+++ Assets/Scripts/OpenLink.cs
@@ -7,17 +7,6 @@ public class OpenLink : MonoBehaviour
 {
     public Color clickedColor;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void Open(string link)
     {
