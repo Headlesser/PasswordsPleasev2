e148edd7ee99e85e15b42369708ed37040f31197
Nathaniel Shipman <nsshipman@gmail.com>
2020-06-28 11:33:02 -0400

    Made a script and some sample text to open resources after game is complete

---
+++ Assets/Scripts/OpenLink.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenLink : MonoBehaviour
+{
+    public Color clickedColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Open(string link)
+    {
+        Application.OpenURL(link);
+        GetComponent<Text>().color = clickedColor;
+    }
+}
